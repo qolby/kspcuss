2fc99170773def15533795c524fa724b6f78bbdc
qolby <131581224+qolby@users.noreply.github.com>
2026-02-20 05:20:08 +0800

    feat: configure output for static deployment on Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
+// import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // explicitly set output to static for Netlify
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
-  adapter: netlify(),
 });
